9af8219de9b0d16d0f0250e4748977b53df87b7e
Vit Gottwald <vit.gottwald@broadcom.com>
2023-12-01 18:28:13 +0100

    fix building from private nmp registry

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
+#!/usr/bin/env bash
+
 rm ./client/example-dialect-support/server/*.jar
 rm ./client/example-dialect-support/*.vsix
+rm -rf ./client/example-dialect-support/node_modules
 rm ./out/*
 
 echo "STEP 1: Build jar file with a server part"
@@ -7,8 +10,6 @@ mvn -f server/dialect-example/ clean package
 
 echo "STEP 2: Copy jar file to the client package folder"
 cp ./server/dialect-example/target/dialect-example.jar ./client/example-dialect-support/server/
-
-npm -prefix client/example-dialect-support/ i @code4z/cobol-dialect-api  --registry https://packages.broadcom.com/artifactory/brightside/
 
 echo "STEP 3: Build client side"
 npm --prefix ./client/example-dialect-support/ ci
